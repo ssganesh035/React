f5daaac58334ed2bfc5b26cf4747a3d276925aa9
ssganesh <ssganesh035@gmail.com>
2025-04-07 17:36:59 +0530

    Added LogoutBtn

---
+++ 12megaBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/config'
+
+function LogoutBtn() {
+    return (
+        <div>LogoutBtn</div>
+    )
+}
+
+export default LogoutBtn
